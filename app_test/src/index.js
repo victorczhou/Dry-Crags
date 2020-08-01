c2a2811baf8b49e990268b850cfc111ec8c03ac9
victorczhou <victorczhou@gmail.com>
2020-08-01 16:41:22 -0700

    added form

--- app_test/src/index.js
+++ app_test/src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+//const KEY = REACT_APP_GKEY;
+//const WKEY = REACT_APP_WKEY;
+//const MPKEY = REACT_APP_MPKEY;
+const GKEY = "AIzaSyDRw3ERq-3qMhR6jywEv94hcpP8ZzDfDzs" // quota'd and site restricted
+const WKEY = "4af0779321ecfde78a7c8185b611182c"
+const MPKEY = "&key=200175868-ec4e9a92dabd127918e609bf7985e5b2"
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 class MyForm extends React.Component {
 	constructor(props) {
 		super(props);
 		var today = new Date();
-		var todaymonth = today.getMonth();
+		var todaymonth =  months[today.getMonth()];
 		var todaydate = today.getDate();
 
 		this.state = {
@@ -28,7 +36,7 @@ class MyForm extends React.Component {
 	}
 
 	handleSubmit(event) {
-		alert('A name was submitted: ' + this.state.value);
+		alert("Submitted");
 		event.preventDefault();
 	}
 
@@ -39,6 +47,30 @@ class MyForm extends React.Component {
 				Start from: <br/>
 				<input type="text" value={this.state.start} onChange={this.handleChange} />
 				</label>
+				<br/>
+
+				<label>
+				Leave month: <br/>
+				<input type="text" value={this.state.leavemonth} onChange={this.handleChange} />
+				</label>
+				<br/>
+
+				<label>
+				Leave day: <br/>
+				<input type="number" value={this.state.leaveday} onChange={this.handleChange} />
+				</label>
+				<br/>
+
+				<label>
+				Leave time: <br/>
+				<input type="number" value={this.state.leavetime} onChange={this.handleChange} />
+				<select value={this.state.leaveampm} onChange={this.handleChange}>
+					<option value="am">am</option>
+					<option value="pm">pm</option>
+				</select>
+				</label>
+				<br/>
+
 				<br/>
 				<input type="submit" value="Find crags!" />
 			</form>
